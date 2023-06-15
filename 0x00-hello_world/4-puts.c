9f2aa54500b947c12540c56e36e50cf0eb315711
Stanley Diamond <codedcrystal@gmail.com>
2023-06-15 12:33:16 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return : 0 (Success)
+ */
+/* this is to test comment */
+int main(void) {
+    puts("Programming is like building a multilingual puzzle\n");
+
+    return (0);
+}
+
