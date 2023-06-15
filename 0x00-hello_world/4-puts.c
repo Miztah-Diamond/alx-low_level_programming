08a072f864fd7c1b2802eb189cf8c238fa9a00c5
Stanley Diamond <codedcrystal@gmail.com>
2023-06-15 13:19:31 -0700

    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints a line with puts function
- * Return : 0 (Success)
+ * main - Entry point of the program
+ *
+ * Description: A program that prints a line with the puts function
+ *
+ * Return: 0 (Success)
  */
-/* this is to test comment */
-int main(void) {
-    puts("Programming is like building a multilingual puzzle\n");
-
-    return (0);
-}
-
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
