b6c829cc35364c640c901c1a39d9b30fde268f0d
Stanley Diamond <codedcrystal@gmail.com>
2023-06-21 08:39:45 -0700

    trial

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,16 +6,16 @@
  * Return: Always 0.
  */
 		
-int main(void)
+int _main(void)
 {
-	int r;
+	int c;
 	
-	r = _islower('H');
+	c = _islower('H');
 	_putchar(r + '0');
-	r = _islower('o');
+	c = _islower('o');
 	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
+	c = _islower(108);
+	_putchar(c + '0');
 	_putchar('\n');
 	return (0);
 }
