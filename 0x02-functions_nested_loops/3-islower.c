949d5948ad30eee987a13761c9883d1654a5e067
Stanley Diamond <codedcrystal@gmail.com>
2023-06-21 08:43:01 -0700

    trial 2

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -11,9 +11,9 @@ int _main(void)
 	int c;
 	
 	c = _islower('H');
-	_putchar(r + '0');
+	_putchar(c + '0');
 	c = _islower('o');
-	_putchar(r + '0');
+	_putchar(c + '0');
 	c = _islower(108);
 	_putchar(c + '0');
 	_putchar('\n');
